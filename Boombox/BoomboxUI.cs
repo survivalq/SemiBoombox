762f3d18f3fb58085bc3cf3adccef0b155efc4ce
survivalq <colorkiller123@gmail.com>
2025-03-15 13:10:37 +0200

    Remove unused namespaces

--- Boombox/BoomboxUI.cs
+++ Boombox/BoomboxUI.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Photon.Pun;
 
--- Utils/YoutubeDL.cs
+++ Utils/YoutubeDL.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.IO.Compression;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Diagnostics;
